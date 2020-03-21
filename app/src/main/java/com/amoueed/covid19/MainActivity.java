29369b15afe90f8691eec5f0a0450f39c8937ef0
MoueedAhmed <moueedrao@gmail.com>
2020-03-21 23:00:30 +0500

    Implement onActivityResult for cancelled FirebaseUI Auth.

--- app/src/main/java/com/amoueed/covid19/MainActivity.java
+++ app/src/main/java/com/amoueed/covid19/MainActivity.java
@@ -3,6 +3,7 @@ package com.amoueed.covid19;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputFilter;
@@ -205,6 +206,21 @@ public class MainActivity extends AppCompatActivity {
         if (mChildEventListener != null) {
             mMessagesDatabaseReference.removeEventListener(mChildEventListener);
             mChildEventListener = null;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign-in succeeded, set up the UI
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // Sign in was canceled by the user, finish the activity
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
         }
     }
 
